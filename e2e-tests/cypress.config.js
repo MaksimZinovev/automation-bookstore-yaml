b873e5a7bcf33f0e5a4d22671a3e8abd8eef6a51
Maksim Zinovev <maksimzinovevsubmit@gmail.com>
2023-08-16 14:32:09 +1000

    Create multistage pipeline

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -6,7 +6,8 @@ module.exports = defineConfig({
     toConsole: true,
   },
   e2e: {
-    baseUrl: 'http://127.0.0.1:8080',
+    // baseUrl: 'http://127.0.0.1:8080',
+    baseUrl: 'https://mzinbookstore-linux.azurewebsites.net/',
     pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
